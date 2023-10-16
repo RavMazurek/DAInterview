38cb080b03747de0161b0e5fa5cb4f59bdcff6fc
Rafal <rafal.mazurek@vestiacom.com>
2023-10-16 20:36:30 +0200

    Switch to XML layout and remove unused files

--- app/src/main/java/com/ravmaz/dainterview/MainActivity.kt
+++ app/src/main/java/com/ravmaz/dainterview/MainActivity.kt
@@ -1,46 +1,16 @@
 package com.ravmaz.dainterview
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ravmaz.dainterview.ui.theme.DAInterviewTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.ravmaz.dainterview.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DAInterviewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DAInterviewTheme {
-        Greeting("Android")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
